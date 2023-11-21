a60f651091ef75bb00636dbc671779b83e4a9c78
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-11-22 01:20:25 +0600

    make Mongoose interface Schema Model

---
+++ src/app/modules/student/student.Interface.ts
@@ -0,0 +1,41 @@
+import { Schema, model, connect } from 'mongoose';
+// 1. Create an interface representing a document in MongoDB.
+
+export type UserName = {
+  firstName: string;
+  meddleName: string;
+  lastName: string;
+};
+
+export type Guardian = {
+  fatherName: string;
+  fatherOcapetion: string;
+  fatherContractNo: string;
+  matherName: string;
+  matherOcapetion: string;
+  matherContractNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  ocapetion: string;
+  address: string;
+  contractNo: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'Male' | 'Female';
+  dateOfBirth?: string;
+  contractNo: string;
+  emergencyContractNo: string;
+  email: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'block';
+};
---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,56 @@
+import { Schema, model, connect } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.Interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  meddleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOcapetion: { type: String, required: true },
+  fatherContractNo: { type: String, required: true },
+  matherName: { type: String, required: true },
+  matherOcapetion: { type: String, required: true },
+  matherContractNo: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  ocapetion: { type: String, required: true },
+  address: { type: String, required: true },
+  contractNo: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['Male', 'Female'],
+  dateOfBirth: String,
+  contractNo: { type: String, required: true },
+  emergencyContractNo: { type: String, required: true },
+  email: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'block'],
+});
+
+const Student = model<Student>('Student', studentSchema);
