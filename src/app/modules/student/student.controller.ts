3145dec971ddea77f31c305cc592c7d4ba1158ad
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-11-22 12:31:51 +0600

    pactice Get,Post,method in mongoDB

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student.services';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    // will cal service function to send this data
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    // send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is create Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDb();
+    res.status(200).json({
+      success: true,
+      message: 'Student are retrieved Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved Successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentController = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
