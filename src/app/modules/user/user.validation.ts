3a38bbe1d6ab621bec0c810f0380802d85d6a768
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-12-16 23:34:36 +0600

    add user section

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,14 +1,16 @@
 import { z } from 'zod';
 
-const userSchema = z.object({
-  id: z.string(),
+const userValidationSchema = z.object({
   password: z
-    .string()
-    .max(20, { message: 'Password can not be more then 20 characters' }),
+    .string({
+      invalid_type_error: 'Password must be string',
+    })
+    .max(20, { message: 'Password can not be more then 20 characters' })
+    .optional(),
   needsPasswordChange: z.boolean().optional().default(true),
   role: z.enum(['admin', 'user', 'faculty']),
   status: z.enum(['in-progress', 'block']).default('in-progress'),
   isDeleted: z.boolean().optional().default(false),
 });
 
-export default userSchema;
+export default userValidationSchema;
