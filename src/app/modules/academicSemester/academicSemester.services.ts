ab24c2fe2c67ce2c2c0e189b77dad8eb94afe60a
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-12-27 21:50:19 +0600

    emplement another validation logic

--- src/app/modules/academicSemester/academicSemester.services.ts
+++ src/app/modules/academicSemester/academicSemester.services.ts
@@ -1,7 +1,21 @@
-import { TAcademicSemesterCode } from './academicSemester.Interface';
+import { TAcademicSemester } from './academicSemester.Interface';
 import { AcademicSemester } from './academicSemester.model';
 
-const createAcademicSemesterIntoDB = async (payload: TAcademicSemesterCode) => {
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // semester name --> semester code
+  type TAcademicSemesterNameCodeMapper = {
+    [key: string]: string;
+  };
+
+  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    Autumn: '01',
+    Semmar: '02',
+    Fall: '03',
+  };
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
